2ee62159049f3501cf7bf52786c6b79fb9e6029f
ZBOMB <75494994+Zbomb2000@users.noreply.github.com>
2021-04-18 20:44:29 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
  from setuptools import setup
 
  setup(
-   name='Package',
+   name='zenity',
    version='0.1.0',
-   author='Human',
+   author='Zbomb2000',
    author_email='hxl5900@gmail.com',
    packages=['math'],
    scripts=['bin/script1','bin/script2'],
    url='http://pypi.python.org/pypi/PackageName/',
    license='LICENSE.txt',
-   description='An awesome package that does something',
+   description='Zenity support for Python 3 on Linux.',
    long_description=open('README.txt').read(),
    install_requires=[
        "Django >= 1.1.1",
