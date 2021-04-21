5fefd49ecd8d8b51c5752957d3c5ea69815b54f9
ZBOMB <75494994+Zbomb2000@users.noreply.github.com>
2021-04-21 10:47:20 -0400

    Update zenity.py

--- zenity.py
+++ zenity.py
@@ -2,10 +2,15 @@ import subprocess
 import os
 
 cd = os.path.realpath('ZenityPy')
-# /mnt/chromeos/MyFiles/ZenityPy/
 
 def warning(text):
     f = open(cd+"/ZenityFiles/text.txt", "w")
     f.write(str(text))
     f.close()
     subprocess.Popen([cd+"/ZenityFiles/warning.sh"])
+    
+def info(text):
+    f = open(cd+"/ZenityFiles/text.txt", "w")
+    f.write(str(text))
+    f.close()
+    subprocess.Popen([cd+"/ZenityFiles/info.sh"])
