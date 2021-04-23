1eff71ef45cce358fa2ef9e8f79b3f7dead733a2
ZBOMB <75494994+Zbomb2000@users.noreply.github.com>
2021-04-23 12:56:25 -0400

    Update zenity.py

--- zenity.py
+++ zenity.py
@@ -8,9 +8,21 @@ def warning(text):
     f.write(str(text))
     f.close()
     subprocess.Popen([cd+"/ZenityFiles/warning.sh"])
-    
+
 def info(text):
     f = open(cd+"/ZenityFiles/text.txt", "w")
     f.write(str(text))
     f.close()
     subprocess.Popen([cd+"/ZenityFiles/info.sh"])
+
+def question(text):
+    f = open(cd+"/ZenityFiles/text.txt", "w")
+    f.write(str(text))
+    f.close()
+    subprocess.Popen([cd+"/ZenityFiles/question.sh"])
+
+def error(text):
+    f = open(cd+"/ZenityFiles/text.txt", "w")
+    f.write(str(text))
+    f.close()
+    subprocess.Popen([cd+"/ZenityFiles/error.sh"])
