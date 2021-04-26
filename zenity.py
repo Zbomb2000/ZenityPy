898ee321e9519baca4e1bd7df78d9119c42fd814
ZBOMB <75494994+Zbomb2000@users.noreply.github.com>
2021-04-26 09:29:29 -0400

    Update zenity.py

--- zenity.py
+++ zenity.py
@@ -27,8 +27,22 @@ def error(text):
     f.close()
     subprocess.Popen([cd+"/ZenityFiles/error.sh"])
 
+def password():
+    subprocess.Popen([cd+"/ZenityFiles/password.sh"])
+    f = open(cd+"/ZenityFiles/text.txt", "r")
+    wait_var1 = f.read()
+    while (True):
+        f = open(cd+"/ZenityFiles/text.txt", "r")
+        wait_var = f.read()
+        if wait_var != wait_var1:
+            break
+    f = open(cd+"/ZenityFiles/text.txt", "r")
+    password_var = f.read()
+    f.close()
+    size = len(password_var)
+    return password_var[:size - 1]
+
 def help():
     print("")
     print("zenity.info(TEXT) ; Replace 'TEXT' with the message you want to send. Same thing for 'error', 'warning', and 'question'.")
     print("")
-
