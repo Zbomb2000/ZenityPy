e0ec663007fbc12b97ecf324af7a500994674dc7
ZBOMB <75494994+Zbomb2000@users.noreply.github.com>
2021-04-26 15:16:01 -0400

    Update zenity.py

--- zenity.py
+++ zenity.py
@@ -61,6 +61,21 @@ def color_selection():
     size = len(rgb_var)
     return rgb_var[:size - 1]
 
+def file_selection():
+    subprocess.Popen([cd+"/ZenityFiles/file_selection.sh"])
+    f = open(cd+"/ZenityFiles/text.txt", "r")
+    wait_var1 = f.read()
+    while (True):
+        f = open(cd+"/ZenityFiles/text.txt", "r")
+        wait_var = f.read()
+        if wait_var != wait_var1:
+            break
+    f = open(cd+"/ZenityFiles/text.txt", "r")
+    file_path_var = f.read()
+    f.close()
+    size = len(file_path_var)
+    return file_path_var[:size - 1]
+
 def help():
     print("")
     print("zenity.info(TEXT) ; Replace 'TEXT' with the message you want to send. Same thing for 'error', 'warning', and 'question'.")
