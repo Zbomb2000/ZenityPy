3c6ebb1debf2d3a4996d4ebadef7c879c723273d
ZBOMB <75494994+Zbomb2000@users.noreply.github.com>
2021-05-06 20:36:48 -0400

    Added notificaion.sh

---
+++ ZenityFiles/notification.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# This gets the file location of the script:
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+TEXT=$(cat "$SCRIPTPATH/text.txt")
+
+zenity --notification --text="$TEXT"
