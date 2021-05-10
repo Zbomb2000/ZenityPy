86b113d806a9711140d5c2c66e9a9390da561bfc
ZBOMB <75494994+Zbomb2000@users.noreply.github.com>
2021-05-10 14:52:37 -0400

    Update zenity.py

--- zenity.py
+++ zenity.py
@@ -140,4 +140,12 @@ def calendar(text):
 def help():
     print("")
     print("zenity.info(TEXT) ; Replace 'TEXT' with the message you want to send. Same thing for 'error', 'warning', and 'question'.")
+    print("zenity.password() This creates a password messege box. This returns a string.")
+    print("zenity.color_selection() This creates a color selection messege box. This returns an rgb value.")
+    print("zenity.file_selection() This creates a file selection messege box. This returns a file path.")
+    print("zenity.entry(TEXT) This sends a text box the user can fill in input. Replace text with the message that you want to send with the text box.")
+    print("zenity.notification(TEXT) This sends a notification. Replace text with the message that you want to send with the notification.")
+    print("zenity.scale(TEXT) This sends a message box that a number from 0-100 can be chosen. Replace text with the message that you want to send with the message box. This returns and int value from 0-100.")
+    print("zenity.calendar(TEXT) This sends a calendar that the user can pick a day from. Replace text with the message that you want to send with the calendar. This returns a date (10/2/2018).")
     print("")
+    
