8b906c8c8b9d5d555091a1b99f76ffd9d194b657
ZBOMB <75494994+Zbomb2000@users.noreply.github.com>
2021-05-10 08:12:13 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+sudo apt install zenity
+
 # This gets the file location of the script:
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
@@ -13,5 +15,8 @@ chmod +x "$SCRIPTPATH/ZenityFiles/password.sh"
 chmod +x "$SCRIPTPATH/ZenityFiles/color_selection.sh"
 chmod +x "$SCRIPTPATH/ZenityFiles/file_selection.sh"
 chmod +x "$SCRIPTPATH/ZenityFiles/notification.sh"
+chmod +x "$SCRIPTPATH/ZenityFiles/scale.sh"
+chmod +x "$SCRIPTPATH/ZenityFiles/entry.sh"
+chmod +x "$SCRIPTPATH/ZenityFiles/calendar.sh"
 
 echo "Done!"
