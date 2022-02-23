c5455a99002b351ccbde70bb75f7dcf0dca7ef7f
zbom <hxl9500@gmail.com>
2022-02-23 12:41:43 -0500

    Moved everything into /.archive

--- ZenityFiles/file_selection.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# This gets the file location of the script:
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-
-FILE_VAR=$(zenity --file-selection)
-
-echo "$FILE_VAR" > "$SCRIPTPATH/text.txt"
--- ZenityFiles/password.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# This gets the file location of the script:
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-
-PASSWORD_VAR=$(zenity --password)
-
-echo "$PASSWORD_VAR" > "$SCRIPTPATH/text.txt"
