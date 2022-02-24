6efc4833a6a76ecf8ae388c79a25cb0beb866fc2
zbom <hxl9500@gmail.com>
2022-02-24 12:20:37 -0500

    Added message dialogs

--- zenitypy/zenity.py
+++ zenitypy/zenity.py
@@ -6,3 +6,19 @@ def password():
     pw1 = pw_input.decode("utf-8")
     pw2 = pw1[:len(pw1) - 1]
     return pw2
+
+def warning(*args, **kwargs):
+    text1 = kwargs.get('text', "Are you sure you want to proceed?")
+    pw_input = subprocess.check_output(['zenity', '--warning', '--text='+str(text1)])
+
+def info(*args, **kwargs):
+    text1 = kwargs.get('text', "All updates are complete.")
+    info_input = subprocess.check_output(['zenity', '--info', '--text='+str(text1)])
+
+def question(*args, **kwargs):
+    text1 = kwargs.get('text', "Are you sure you want to proceed?")
+    question_input = subprocess.check_output(['zenity', '--question', '--text='+str(text1)])
+
+def error(*args, **kwargs):
+    text1 = kwargs.get('text', "An error has occurred.")
+    error_input = subprocess.check_output(['zenity', '--error', '--text='+str(text1)])
